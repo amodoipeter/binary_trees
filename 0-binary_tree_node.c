96444e834eaced9a3768c3d24c8b16e870edec02
Amodoi Peter <amodoipeter@gmail.com>
2022-08-25 23:55:49 +0300

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,13 +6,12 @@
  *@value : int to be put in the new node
  *Return: The returning node;
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
 
     new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
+    if (!(new_node))
         return (NULL);
     new_node->n = value;
     new_node->parent = parent;
